e5b9d3e889385520583c9edb2b74f7f0c944b640
WouterWijnands <wouterwijnands@protonmail.ch>
2020-07-11 17:50:02 +0200

    Created proper random position of character on refresh function, added character click with random position function

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,22 @@
 /* eslint-disable */
 
+let character = document.getElementsByClassName("character");
 
-function randomPosition() {
-  let positionY = Math.round(Math.random() * document.body.clientHeight);
-  let positionX = Math.round(Math.random() * document.body.clientWidth);
-  let element = document.getElementsByClassName("character");
-  console.log(element.positionX);
-  element.style.positionY = positionY;
-  element.style.positionX = positionX;
+function randomPosition(character) {
+  let randomNumberY = Math.round(Math.random() * document.body.clientHeight);
+  let randomNumberX = Math.round(Math.random() * document.body.clientWidth);
+  let characterHeight = character[0].clientHeight;
+  let characterWidth = character[0].clientWidth;
+  let randomTop = randomNumberY - characterHeight;
+  let randomLeft = randomNumberX - characterWidth;
+  character[0].style.top = (randomTop + "px");
+  character[0].style.left = (randomLeft + "px");
 }
 
+character[0].onclick = function() {
+  randomPosition(character);
+};
+
 window.onload = function() {
-    randomPosition();
+    randomPosition(character);
 }
