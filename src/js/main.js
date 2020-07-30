62ef255e0129cb8f44f14f0de2bd2c8842e8751c
WouterDeveloper <wouterwijnands@protonmail.com>
2020-07-30 12:20:01 +0200

    Implemented click function, added basics for score

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 /* eslint-disable */
 
 let character = document.getElementsByClassName("character");
+let background = document.getElementById("app");
+let body = document.getElementsByTagName('body')[0];
+let score = 0;
 
 function randomPosition(character) {
   let randomNumberY = Math.round(Math.random() * document.body.clientHeight);
@@ -13,9 +16,45 @@ function randomPosition(character) {
   character[0].style.left = (randomLeft + "px");
 }
 
+function showScore(){
+
+}
+
+function addScore(score){
+  ++score;
+  console.log(score)
+}
+
+function gameOver(){
+
+}
+
 character[0].onclick = function() {
   randomPosition(character);
+  addScore();
 };
+
+body.onclick = function() {
+  gameOver();
+};
+
+document.getElementsByClassName('character')[0]
+.addEventListener('click', function (event) {
+  console.log('hoi');
+});
+
+document.addEventListener('click', function (event) {
+  console.log(event.target.closest);
+  console.log('body');
+});
+
+
+// character.addEventListener("click", function(event){
+//   if (!(event.target).closest(character).length) {
+//     // ... clicked on the 'body', but not inside of #menutop
+//     console.log("je bent af");
+//   }
+// }); 
 
 window.onload = function() {
     randomPosition(character);
