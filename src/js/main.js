5d8a46c371e82c4f2d992aec5018f981e5bccc2d
WouterDeveloper <wouterwijnands@protonmail.com>
2020-08-17 15:16:11 +0200

    Added character removal and append instead of move, added timer on which character spawns, still have to add reset timer

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,36 @@
 /* eslint-disable */
 
-let character = document.getElementsByClassName("character");
+let character = document.getElementsByClassName("character")[0];
 let background = document.getElementById("app");
 let body = document.getElementsByTagName('body')[0];
 let score = 0;
 
 function spawnCharacterAtRandomPosition(character) {
-  let randomNumberY = Math.round(Math.random() * document.body.clientHeight);
-  let randomNumberX = Math.round(Math.random() * document.body.clientWidth);
-  let characterHeight = character[0].clientHeight;
-  let characterWidth = character[0].clientWidth;
-  let randomTop = randomNumberY - characterHeight;
-  let randomLeft = randomNumberX - characterWidth;
-  character[0].style.top = (randomTop + "px");
-  character[0].style.left = (randomLeft + "px");
+  let characterHeight = character.clientHeight;
+  let characterWidth = character.clientWidth;
+  let randomTopPosition = Math.round(Math.random() * (document.body.clientHeight - characterHeight));
+  let randomLeftPosition = Math.round(Math.random() * (document.body.clientWidth) - characterWidth);
+  character.style.top = (randomTopPosition + "px");
+  character.style.left = (randomLeftPosition + "px");
+  background.appendChild(character);
 }
 
-function deleteCharacter() {
-  var character = document.getElementsByClassName("character");
+function deleteCharacter(character) {
   character.remove();
+}
+
+function timer(character){
+  const timeLeftDisplay = document.getElementById("time");
+  let timeLeft = 2;
+  setInterval(function(){
+    if(timeLeft <= 0){
+      clearInterval(timeLeft = 2);
+      spawnCharacterAtRandomPosition(character);
+    }
+    timeLeftDisplay.innerHTML = timeLeft;
+    timeLeft -=1;
+  }, 1000)
+  //time = setInterval(spawnCharacterAtRandomPosition(character), 3000);
 }
 
 function showScore(){
@@ -27,7 +39,7 @@ function showScore(){
 
 function addScore(score){
   ++score;
-  console.log(score)
+ // console.log(score)
 }
 
 function gameOver(){
@@ -44,12 +56,19 @@ body.onclick = function() {
   console.log("Je bent af");
 };
 
-character[0].addEventListener('click', function(){
+character.addEventListener('click', function(){
   event.stopImmediatePropagation();
   deleteCharacter(character);
+  timer(character);
+  spawnCharacterAtRandomPosition(character);
+  
+  //resetTimer();
+  // is deleteCharacter() en dan in die functie var character = getelementbyclass, character.remove beter
+  /// Of hoe ik het nu doe?
   addScore();
 })
 
 window.onload = function() {
-  spawnCharacterAtRandomPosition(character);
+  timer(character);
+
 }
